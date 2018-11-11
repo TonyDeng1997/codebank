91f6339724a9327b0eca139875d87edd458f2936
xiaofeng li <xlics05@gmail.com>
2018-11-10 20:09:12 -0500

    new examples and doc update

---
+++ src/com/webbertech/java/ExecutorServiceExample.java
@@ -0,0 +1,46 @@
+package com.webbertech.java;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+
+class WorkerThread implements Runnable {
+	private String message;
+
+	public WorkerThread(String s) {
+		this.message = s;
+	}
+
+	public void run() {
+		System.out.println(Thread.currentThread().getName() + " (Start) message = " + message);
+		processmessage();// call processmessage method that sleeps the thread for 2 seconds
+		System.out.println(Thread.currentThread().getName() + " (End)");// prints thread name
+	}
+
+	private void processmessage() {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+public class ExecutorServiceExample {
+	public ExecutorServiceExample() {
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		ExecutorService executor = Executors.newFixedThreadPool(5);// creating a pool of 5 threads
+		for (int i = 0; i < 10; i++) {
+			Runnable worker = new WorkerThread("" + i);
+			executor.execute(worker);// calling execute method of ExecutorService
+		}
+		executor.shutdown();
+		while (!executor.isTerminated()) {
+			System.out.println("not yet");
+			Thread.sleep(1000);
+		}
+
+		System.out.println("Finished all threads");
+	}
+}
